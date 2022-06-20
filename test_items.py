91bd11808e9279b442a4517f8c8b415f524a89f4
SyberiaK <syberiakey@gmail.com>
2022-06-20 15:04:46 +0500

    Changed class name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_basket(browser):
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button
